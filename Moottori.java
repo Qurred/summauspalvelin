f47990c31d0db057a9e8ee6a7d95d98a76874caf
Juho Kuusinen <qurred@gmail.com>
2016-11-13 19:18:18 +0200

    Pieniä lisäyksiä
    Pieniä lisäyksiä ja vähäistä muuttujien määrittelyä

--- Moottori.java
+++ Moottori.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class Moottori{
 
+	public static final int KUUNNELTAVAPORTTI = 20050;
+	public static ArrayList<Summauspalvelu> sp;
+	
+	/**
+	 * Harjoitustyössä yhdistettävä portti on 3126, poista myöhemmässä vaiheessa
+	 */
+	public static void main(String[] args){
+		if(args.length < 2){
+			System.err.println("Annettiin "+args.length+" parametria vaikka tarvitaan kaksi.\n"
+					+ "Syötä parametrit muotoa:\n[Osoite] [Portti]");
+			System.exit(0);
+		}
+		System.out.println("Käynnistetään summauspalvelua..");
+		sp = new ArrayList<>();
+		lahetaUDP(args[0], Integer.parseInt(args[1]));
+	}
+	
+	public static void lahetaUDP(String osoite, int portti){
+		System.out.println("Lähetetään UDP-pakettiä osoitteeseen "+osoite+":"+portti);
+		System.out.println("Nyt pitäisi odottaa 5 s yhteyden muodostumista\nJos yhteyttä ei muodosteta niin paketti lähetetään uudestaan");
+		
+	}
+	
+	public static void luoSummauspalvelut(){
+		
+	}
 }
--- Summauspalvelu.java
+++ Summauspalvelu.java
@@ -1,3 +1,29 @@
-public class Summauspalvelu{
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Summauspalvelu implements Runnable{
+
+	private static final int MAKSIMIODOTUSAIKA = 5000; //5s
+	private int summa;
+	private int lukumaara;
+	private ServerSocket ss;
+	
+	public Summauspalvelu(int portti){
+		try {
+			this.ss = new ServerSocket(portti);
+			this.ss.setSoTimeout(MAKSIMIODOTUSAIKA);
+			this.summa = 0;
+			this.lukumaara = 0;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void run() {
+		// Jaa'a		
+	}
+	
+	
 
 }
