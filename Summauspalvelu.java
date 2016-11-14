9c13c871138d02c3d6ed17b63c594cc8d8e0879e
Emirle <e.lepisto@hotmail.com>
2016-11-14 21:21:35 +0200

    summauspalvelimen aloittelua

--- Summauspalvelu.java
+++ Summauspalvelu.java
@@ -4,24 +4,40 @@ import java.net.ServerSocket;
 public class Summauspalvelu implements Runnable{
 
 	private static final int MAKSIMIODOTUSAIKA = 5000; //5s
-	private int summa;
-	private int lukumaara;
+	private static int summa;
+	private static int lukumaara;
+	private static int suurin;
 	private ServerSocket ss;
 	
 	public Summauspalvelu(int portti){
 		try {
 			this.ss = new ServerSocket(portti);
 			this.ss.setSoTimeout(MAKSIMIODOTUSAIKA);
-			this.summa = 0;
-			this.lukumaara = 0;
+			summa = 0;
+			lukumaara = 0;
+			suurin = 0;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 	}
+	public synchronized int suurinSumma(int luku){
+		if (luku < suurin){
+			return luku;
+		}
+			else{
+				return suurin;
+		}
+    }
+	public synchronized int lukujenMaara(){
+		return lukumaara +1;
+	}
+	public synchronized int lukujenSumma(int luku){
+		return summa + luku;
+	}
 	
 	public void run() {
-		// Jaa'a		
+		// Jaa'a 		
 	}
 	
 	
