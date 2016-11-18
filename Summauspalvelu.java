dc5bfe652bcfa8c1e0ff68e17342236a70433e56
Emirle <e.lepisto@hotmail.com>
2016-11-18 17:04:21 +0200

    Täysin epämääräistä

--- Summauspalvelu.java
+++ Summauspalvelu.java
@@ -1,19 +1,25 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Summauspalvelu implements Runnable{
 
 	private static final int MAKSIMIODOTUSAIKA = 20000; //5s
+	private static int luku;
 	private static int summa;
-	private static int lukumaara;
-	private static int suurin;
 	private ServerSocket ss;
 	private Socket asiakas;
 	private ObjectInputStream ois;
 	private ObjectOutputStream ous;
+	private OutputStream oS;
+	private InputStream iS;
+	boolean kaynnissa;
+	ArrayList<Integer> lista;
 	
 	public Summauspalvelu(int portti){
 		try {
@@ -21,42 +27,59 @@ public class Summauspalvelu implements Runnable{
 			this.ss.setSoTimeout(MAKSIMIODOTUSAIKA);
 			this.asiakas = new Socket();
 			summa = 0;
-			lukumaara = 0;
-			suurin = 0;
+			luku = 0;
+			kaynnissa = true;
+			lista = new ArrayList<Integer>();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 	}
-	public synchronized int suurinSumma(int luku){
-		if (luku < suurin){
-			suurin = luku;
-			 return luku;
-		}
-			else{
-			 return suurin;
+	public synchronized int suurinSumma(ArrayList<Integer> suurin){
+		for (int i = 0; i < suurin.size(); i++){
+		if (suurin.get(i) > luku){
+			luku = suurin.get(i);
 		}
     }
-	public synchronized int lukujenMaara(){
-		lukumaara = lukumaara++;
-		return lukumaara;
-	}
-	public synchronized int lukujenSumma(int luku){
-		summa = summa + luku;
-		return summa;
+		return luku;
 	}
 	
-	public void run() {
+	public synchronized int lukujenMaara(ArrayList<Integer> maara){
+		return maara.size();
+	}
+	public synchronized int lukujenSumma(ArrayList<Integer> summat){
+		for (int i = 0; i < summat.size(); i++){
+		summa = summa + summat.get(i);
+	}
+		return summa;
+	}
 
+	public void run() {
+		
 		try {
 			asiakas = ss.accept();
-			this.ois = new ObjectInputStream(asiakas.getInputStream());
-			this.ous = new ObjectOutputStream(asiakas.getOutputStream());
+			iS = asiakas.getInputStream();
+	        oS = asiakas.getOutputStream();
+	        ous = new ObjectOutputStream(oS);
+	        ois = new ObjectInputStream(iS);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	while (kaynnissa){
+	
+		try {
+			int luku = ois.readInt();
+			if (luku == 0){
+				kaynnissa = false;
+				break;
+			}
+			lista.add(luku);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		try {
 			asiakas.close();
 		} catch (IOException e) {
@@ -70,7 +93,7 @@ public class Summauspalvelu implements Runnable{
 			//Käytään arvolla määritellyt metodit läpi
 			//jne...
 	
-
+   }
 		
 		
 	}
